a1742c1235df95f327ff9c49560854e034daf4b8
WIMAX COMPUTECH <WIMAX COMPUTECH@TASHTO>
2023-01-30 01:50:07 +0100

    Add project files.

---
+++ last ex/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+
+namespace Ahmad_Make
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 'start' to begin writing in Captain's journal.");
+            string input = Console.ReadLine();
+
+            if (input == "start")
+            {
+                Console.WriteLine("Enter 'stop' to end writing in Captain's journal.");
+                string content = "";
+
+                while (input != "stop")
+                {
+                    input = Console.ReadLine();
+                    if (input != "stop")
+                    {
+                        content += input + "\n";
+                    }
+                }
+
+                string fileName = DateTime.Now.ToString("yyyy-MM-dd") + ".txt";
+                File.WriteAllText(fileName, "Captain's log\nStardate " + DateTime.Now.ToString("yyyy-MM-dd") + "\n" + content);
+                Console.WriteLine("Journal entry saved as " + fileName);
+            }
+        }
+    }
+}
+
